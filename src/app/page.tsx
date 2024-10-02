0c992ed37f9907cf153a545cc3fd87848339944f
ezequiel stom <ezequielstom@gmail.com>
2024-10-02 19:08:59 -0300

    Refactorizar estilos de componente de entrada de datos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Bot } from "@/components/bot";
 export default function Home() {
   return (
-    <div className="bg-transparent w-fit h-fit">
+    <div className="bg-transparent w-screen h-screen">
       <Bot />
     </div>
   );
