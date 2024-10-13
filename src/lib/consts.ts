e1c3c5a7783f7f355ec939cc27d0b7f2731a624c
ezequiel stom <ezequielstom@gmail.com>
2024-10-13 20:54:46 -0300

    Refactorizar estilos de componente de entrada de datos y eliminar código innecesario

---
+++ src/lib/consts.ts
@@ -0,0 +1,212 @@
+export const keywordResponses = [
+	{
+		keywords: ["Cacta", "Cacta software", "app Cacta"],
+		response:
+			"Cacta es una app de sustentabilidad que automatiza, calcula y reporta el impacto ambiental de tu empresa.",
+	},
+	{
+		keywords: [
+			"Más que un Software",
+			"Cacta es más que un software",
+			"más que un software agrícola",
+		],
+		response:
+			"Cacta es mucho más que un software; es la puerta al futuro de la industria agrícola, ayudando a automatizar procesos y mejorar la eficiencia.",
+	},
+	{
+		keywords: [
+			"Vista 360",
+			"vista 360 Cacta",
+			"información en una pantalla",
+		],
+		response:
+			"Cacta ofrece una vista 360, centralizando toda tu información en una pantalla para simplificar el análisis ambiental y mejorar la toma de decisiones.",
+	},
+	{
+		keywords: ["Eficiencia", "mejorar eficiencia", "más eficiencia"],
+		response:
+			"Con Cacta puedes mejorar la eficiencia de tu empresa, eliminando procesos manuales y automatizando la captura de datos.",
+	},
+	{
+		keywords: [
+			"Informes automáticos",
+			"reportes automáticos",
+			"generación de informes",
+		],
+		response:
+			"Cacta genera informes automáticos, lo que te permite estar al tanto de regulaciones internacionales y mantener reportes actualizados sin esfuerzo.",
+	},
+	{
+		keywords: [
+			"Indicadores ambientales",
+			"12 indicadores claves",
+			"mide impacto ambiental",
+		],
+		response:
+			"Cacta ofrece 12 indicadores clave para entender y comunicar el impacto ambiental de tu empresa, desde cambio climático hasta agotamiento de recursos.",
+	},
+	{
+		keywords: [
+			"Monitoreo",
+			"alertas en tiempo real",
+			"monitoreo ambiental",
+		],
+		response:
+			"Con Cacta puedes monitorear tu impacto ambiental en tiempo real y recibir alertas configurables para actuar rápidamente.",
+	},
+	{
+		keywords: [
+			"Monitoreo por establecimiento",
+			"control por establecimiento",
+			"monitoreo por campo",
+		],
+		response:
+			"Cacta permite el monitoreo por establecimiento, asegurando un control completo de las operaciones, recursos y rendimientos en cada campo.",
+	},
+	{
+		keywords: [
+			"Monitoreo por producto",
+			"trazabilidad de productos",
+			"monitoreo por producto",
+		],
+		response:
+			"Cacta proporciona trazabilidad del uso de recursos y las emisiones generadas por tus actividades, ayudando a mejorar la eficiencia y la sustentabilidad.",
+	},
+	{
+		keywords: [
+			"Declaraciones ambientales",
+			"EPD",
+			"declaraciones ambientales de producto",
+		],
+		response:
+			"Cacta incluye Declaraciones Ambientales de Producto, mostrando el impacto ambiental a lo largo de todo el ciclo de vida del material.",
+	},
+	{
+		keywords: [
+			"Cálculo medioambiental",
+			"cálculo de huella ambiental",
+			"cálculo de impacto ambiental",
+		],
+		response:
+			"Cacta centraliza toda tu información en una sola pantalla, permitiendo identificar de forma sencilla los principales contribuyentes de tu huella ambiental.",
+	},
+	{
+		keywords: [
+			"Cadena de valor",
+			"impacto en la cadena de valor",
+			"indicadores cadena de valor",
+		],
+		response:
+			"Cacta desagrega los indicadores de impacto para cada actor de tu cadena de valor, facilitando el reporte según los estándares internacionales.",
+	},
+	{
+		keywords: [
+			"Reportes EPD",
+			"declaraciones ambientales EPD",
+			"reportes de sostenibilidad",
+		],
+		response:
+			"Cacta ofrece reportes EPD (Declaraciones Ambientales de Producto) siguiendo la normativa ISO-14025, asegurando que cumples con las regulaciones ambientales.",
+	},
+	{
+		keywords: ["Eco Score", "indicador Eco Score", "cálculo Eco Score"],
+		response:
+			"Cacta calcula tu Eco Score, un único indicador de performance ambiental basado en benchmarks internacionales, para comunicar tu progreso de manera confiable.",
+	},
+	{
+		keywords: ["Misión", "propósito de Cacta", "misión de Cacta"],
+		response:
+			"Nuestra misión es apoyar a la industria primaria en su transformación hacia un futuro sustentable.",
+	},
+	{
+		keywords: [
+			"Decisiones informadas",
+			"tomar decisiones informadas",
+			"información para decisiones",
+		],
+		response:
+			"Nuestro principal objetivo es que las empresas tomen decisiones informadas, reduciendo su impacto ambiental y optimizando su eficiencia operativa.",
+	},
+	{
+		keywords: [
+			"Conexión a sistemas de gestión",
+			"integración con sistemas de gestión",
+			"conexión a software",
+		],
+		response:
+			"Cacta se conecta a tu sistema de gestión, obteniendo información confiable y con trazabilidad.",
+	},
+	{
+		keywords: [
+			"Desarrollo agrícola",
+			"software agrícola",
+			"solución agrícola",
+		],
+		response:
+			"Cacta es software desarrollado por y para agricultores bajo la metodología de Análisis de Ciclo de Vida (LCA).",
+	},
+	{
+		keywords: [
+			"Gestión sustentable",
+			"gestión ambiental",
+			"automación de gestión",
+		],
+		response:
+			"Cacta ofrece paneles e informes automatizados que harán de tu gestión sustentable una labor simple y eficaz.",
+	},
+	{
+		keywords: [
+			"Registro de emisiones",
+			"emisiones al medio ambiente",
+			"registro ambiental",
+		],
+		response:
+			"Cacta cuenta con un algoritmo optimizado que registra más de 70.000 emisiones al suelo, aire y agua, y 27.000 registros ambientales.",
+	},
+	{
+		keywords: [
+			"Certificación",
+			"solución certificada",
+			"cumplimiento de estándares",
+		],
+		response:
+			"Cacta es una solución certificada, alineada a los estándares europeos y la normativa ISO 14040/44, siendo la herramienta más completa del mercado.",
+	},
+	{
+		keywords: [
+			"Personalización",
+			"herramienta personalizada",
+			"solución a medida",
+		],
+		response:
+			"Cacta es una herramienta customizada a tus necesidades, adecuada para pequeños, medianos y grandes productores.",
+	},
+	{
+		keywords: [
+			"Sustentabilidad en la agricultura",
+			"agricultura sustentable",
+			"producción sustentable",
+		],
+		response:
+			"Cacta promueve la sustentabilidad en la agricultura, ayudando a los productores a reducir su impacto ambiental mientras optimizan sus operaciones.",
+	},
+];
+
+export const allowedTopics = [
+	"cacta",
+	"energías limpias",
+	"sustentabilidad",
+	"medio ambiente",
+];
+
+
+ export const fallbackResponses = [
+		"Parece que tu pregunta no está en mis áreas de especialización. Sin embargo, puedo ofrecerte información sobre energías limpias, sustentabilidad, o el funcionamiento de nuestra aplicación. ¿Te gustaría explorar alguno de estos temas?",
+		"No tengo información específica sobre ese asunto, pero estoy aquí para ayudarte con temas relacionados con energías renovables, la eficiencia ambiental, o el impacto de la agricultura en el medio ambiente. ¿Te interesa alguno de ellos?",
+		"Lamento no entender tu pregunta. ¿Podrías darme más detalles o ser más específico sobre lo que necesitas? Estoy aquí para ayudarte.",
+		"No tengo información sobre ese tema en particular. Pero puedo ofrecerte información sobre el uso sostenible de recursos, la gestión ambiental, o cómo nuestra aplicación puede ayudarte a optimizar tu impacto. ¿Te gustaría saber más?",
+		"¿Podrías reformular tu pregunta o darme un poco más de contexto? Así podré ofrecerte una respuesta más precisa y útil.",
+		"Lo siento, no tengo la información que buscas. Pero si estás interesado en conocer más sobre la sostenibilidad, el análisis de ciclo de vida, o cómo mejorar tu eficiencia operativa, házmelo saber.",
+		"Puedo ayudarte mejor si me proporcionas más detalles. ¿Qué aspecto de la sostenibilidad o la eficiencia ambiental te gustaría que exploremos juntos?",
+		"No tengo datos sobre ese tema. Sin embargo, puedo ayudarte a entender cómo gestionar tus emisiones, o cómo utilizar nuestro software para mejorar tu impacto ambiental. ¿Te gustaría eso?",
+ ];
