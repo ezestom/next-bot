2944c70fd923f69ba360c7df4f8534b375746e10
ezequiel stom <ezequielstom@gmail.com>
2024-10-14 20:02:36 -0300

    Refactorizar estilos de componente de entrada de datos y normalizar palabras clave en el componente de bot

--- src/components/bot.tsx
+++ src/components/bot.tsx
@@ -1,24 +1,31 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { useChat } from 'ai/react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { X, BotIcon, Send } from 'lucide-react'
-import { getLlamaCompletion } from '@/lib/llama' // Importa tu función de IA
+import { getLlamaCompletion } from '@/lib/llama'
 import "./bot.css"
 import { keywordResponses, allowedTopics, fallbackResponses } from '@/lib/consts'
 
+let messageCounter = 0;
+
+
 export function Bot() {
-  const { messages } = useChat()
   const [isWindowOpen, setIsWindowOpen] = useState(false)
   const [isAnimating, setIsAnimating] = useState(false)
   const [prompt, setPrompt] = useState('') // Estado para capturar el input del usuario
-  const [botMessages, setBotMessages] = useState(messages) // Estado para manejar los mensajes del bot
-  const [loading, setLoading] = useState(false); // Estado para manejar la carga
+  type BotMessage = {
+    id: string;
+    role: 'user' | 'assistant';
+    content: string;
+  };
 
+  const [botMessages, setBotMessages] = useState<BotMessage[]>([]) // Estado para manejar los mensajes del bot
+  const [loading, setLoading] = useState(false); // Estado para manejar la carga
+  const [hasGreeted, setHasGreeted] = useState(false); // Para verificar si ya saludó
 
   // Función para obtener la respuesta de IA
   const completion = async () => {
@@ -26,7 +33,6 @@ export function Bot() {
     return response
   }
 
-  // Normaliza el prompt antes de agregar el mensaje
   const normalizePrompt = (prompt: string) => {
     return prompt
       .trim()
@@ -35,92 +41,101 @@ export function Bot() {
       .replace(/[\u0300-\u036f]/g, ""); // Elimina los acentos
   }
 
-  // Función para validar el prompt del usuario que contenga los siguientes tópicos
   const isValidPrompt = (prompt: string) => {
     return allowedTopics.some(topic => prompt.includes(topic.toLowerCase()));
   };
 
-  // Función que genera una respuesta alternativa si el prompt es inválido
   const generateFallbackResponse = () => {
     return fallbackResponses[Math.floor(Math.random() * fallbackResponses.length)];
   }
 
   const handleToggleWindow = () => {
-    const chatInput = document.getElementById('chat-input')
-    if (chatInput) chatInput.focus({ preventScroll: true })
     setIsAnimating(true)
     setIsWindowOpen(prev => !prev)
-    // limpia el chat si se cierra
-    if (!isWindowOpen) setBotMessages([])
+
+    if (!isWindowOpen) {
+      // Limpiar el chat si se cierra
+      setBotMessages([])
+
+      // Saludar al abrir el chat por primera vez
+      if (!hasGreeted) {
+        setBotMessages([{ id: Date.now().toString(), role: 'assistant', content: "¡Hola! Soy Cacta AI Assistant. Puedo brindarte más información sobre nuestra App" }]);
+        setHasGreeted(true);
+      }
+    }
   }
+
+  const generateUniqueId = () => {
+    return `${Date.now()}-${messageCounter++}`;
+  };
+
 
   const handleSendMessage = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (prompt.trim() === '') return; // Evitar enviar mensajes vacíos
+    if (prompt.trim() === '') return;
 
+    setLoading(true); // Iniciar el indicador de escritura
     const normalizedPrompt = normalizePrompt(prompt);
 
-    // Agregar el mensaje del usuario
-    setBotMessages(prev => [...prev, { id: Date.now().toString(), role: 'user', content: prompt }]);
+    // Agregar el mensaje del usuario con ID único
+    setBotMessages(prev => [...prev, { id: generateUniqueId(), role: 'user', content: prompt }]);
 
-    // Limpiar el input
     setPrompt('');
 
-    // Verificar si el prompt contiene alguna palabra clave
     const keywordResponse = keywordResponses.find(item => item.keywords.some(keyword => normalizedPrompt.includes(normalizePrompt(keyword))));
 
     if (keywordResponse) {
-      // Si encuentra una coincidencia con una palabra clave, mostrar la respuesta predefinida
-      setBotMessages(prev => [
-        ...prev,
-        { id: Date.now().toString(), role: 'assistant', content: keywordResponse.response }
-      ]);
-      return; // Salir de la función
+      setTimeout(() => { // Simular un retraso de 2 segundos
+        setBotMessages(prev => [
+          ...prev,
+          { id: generateUniqueId(), role: 'assistant', content: keywordResponse.response }
+        ]);
+        setLoading(false); // Finalizar el indicador de escritura
+      }, 2000);
+      return;
     }
 
-    // Verificar si el prompt contiene temas permitidos
     if (!isValidPrompt(normalizedPrompt)) {
-      // Si el prompt no es válido, generar una respuesta alternativa
       const fallbackResponse = generateFallbackResponse();
-      setBotMessages(prev => [
-        ...prev,
-        { id: Date.now().toString(), role: 'assistant', content: fallbackResponse }
-      ]);
-      return; // Salir de la función
+      setTimeout(() => { // Simular un retraso de 2 segundos
+        setBotMessages(prev => [
+          ...prev,
+          { id: generateUniqueId(), role: 'assistant', content: fallbackResponse }
+        ]);
+        setLoading(false); // Finalizar el indicador de escritura
+      }, 2000);
+      return;
     }
 
-    // Establecer estado de carga para simular typing
-
-    setLoading(true);
     try {
-      // Obtener respuesta de IA
-      await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 1 segundo
-
-      // Obtener respuesta de IA
-      const response = await completion();
-
-      // Agregar respuesta del bot
-      setBotMessages(prev => [...prev, { id: Date.now().toString(), role: 'assistant', content: response }]);
+      // Simular el tiempo de respuesta de la IA
+      setTimeout(async () => {
+        const response = await completion();
+        setBotMessages(prev => [...prev, { id: generateUniqueId(), role: 'assistant', content: response }]);
+        setLoading(false); // Finalizar el indicador de escritura
+      }, 2000); // Retraso de 2 segundos
 
     } catch (error) {
-      // Manejar cualquier error
       console.error("Error fetching AI response:", error);
-      setBotMessages(prev => [
-        ...prev,
-        { id: Date.now().toString(), role: 'assistant', content: "Lo siento, hubo un problema al procesar tu solicitud. Inténtalo de nuevo más tarde." }
-      ]);
-    } finally {
-      // Restablecer el estado de carga
-      setLoading(false);
+      setTimeout(() => {
+        setBotMessages(prev => [
+          ...prev,
+          { id: generateUniqueId(), role: 'assistant', content: "Lo siento, hubo un problema al procesar tu solicitud. Inténtalo de nuevo más tarde." }
+        ]);
+        setLoading(false); // Finalizar el indicador de escritura
+      }, 3000); // Retraso de 3 segundos
     }
   };
 
+
   useEffect(() => {
-    if (isAnimating) {
-      const timer = setTimeout(() => setIsAnimating(true), 500) // Match this with the CSS transition duration
-      return () => clearTimeout(timer)
+    const scrollArea = document.getElementById('scroll');
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight;
     }
-  }, [isAnimating])
+  }, [botMessages, loading]);
+
+
 
   return (
     <div className="fixed inset-0 sm:bottom-4 sm:right-4 sm:inset-auto flex items-end justify-end">
@@ -152,7 +167,7 @@ export function Bot() {
             <form onSubmit={handleSendMessage} className="flex w-full space-x-2">
               <Input
                 value={prompt}
-                onChange={(e) => setPrompt(e.target.value)} // Capturar el input del usuario
+                onChange={(e) => setPrompt(e.target.value)}
                 placeholder="Can I help you with something?"
                 className="flex-grow focus-visible:ring-1 focus-visible:ring-black "
                 id='chat-input'
